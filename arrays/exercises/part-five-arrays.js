fafc1883b37efdc93d18de6651f9bf1b7dd04f24
Therealemichelle1503 <Therealemichelle1503@gmail.com>
2024-01-18 17:12:33 -0600

    finished array exercises

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,21 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
+console.log(str.split());
+console.log(str.split('e'));
+console.log(str.split(' '));
+console.log(str.split(''));
 
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
+console.log(arr.join());    // adds commas between the letters
+console.log(arr.join('a')); // adds an 'a' between the letters
+console.log(arr.join(' ')); // adds a space between the letters
+console.log(arr.join(''));  // removes spaces between the letters 
 
 //3) Do split or join change the original string/array?
+// does not change the array's content but it does change the format
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+
+console.log(cargoHold.split(',').sort().join(','));
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,21 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+console.log(holdCabinet1.concat(holdCabinet2));
+console.log(holdCabinet1);
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+let objects = holdCabinet1.slice(0, 2);
+console.log(objects);
+
+let objects2 = holdCabinet2.slice(1, 3);
+console.log(objects2);
+//slice does not impact the original array since we are creating a new array essentially
 
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse();
+console.log(holdCabinet1);
+
+holdCabinet2.sort();
+console.log(holdCabinet2);
+//the first method reverses the order of the original array while the seconds sorts the array in a logical order. Both of these methods alter the original arrays. 
