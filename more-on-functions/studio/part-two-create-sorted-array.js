8bdefcf8a1dad6b8b2afa49b944af41c1803c397
Therealemichelle1503 <Therealemichelle1503@gmail.com>
2024-01-29 21:22:21 -0600

    finished studio

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,6 +1,6 @@
-function findMinValue(arr){
+function findMin(arr){
   let min = arr[0];
-  for (i = 0; i < arr.length; i++){
+  for (i = 1; i < arr.length; i++){
     if (arr[i] < min){
       min = arr[i];
     }
@@ -8,6 +8,15 @@ function findMinValue(arr){
   return min;
 }
 
+function sortNums(arr) {
+  let sortedArr = [];
+  while (arr.length > 0) {
+    let min = findMin(arr);
+    sortedArr.push(min);
+    arr.splice(arr.indexOf(min), 1);
+  }
+  return sortedArr;
+}
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
 /*Within the function:
@@ -27,3 +36,11 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+console.log(sortNums(nums1));
+console.log(sortNums(nums2));
+console.log(sortNums(nums3));
+
+console.log(nums1);
+console.log(nums2);
+console.log(nums3);
